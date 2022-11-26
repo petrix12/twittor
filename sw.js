8fafb14958ba590b616ffb0f94a92e9878d0c209
Pedro Bazó <bazo.pedro@gmail.com>
2022-11-26 18:11:39 -0400

    Modificaciones para iPhone X+

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE      = 'static-v3';
-const DYNAMIC_CACHE     = 'dynamic-v1';
+const STATIC_CACHE      = 'static-v4';
+const DYNAMIC_CACHE     = 'dynamic-v2';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -39,6 +39,10 @@ self.addEventListener('activate', e => {
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
             //if (key !== CACHE_STATIC_NAME || key !== CACHE_DYNAMIC_NAME || key !== CACHE_INMUTABLE_NAME) {
+                return caches.delete(key);
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
         });
